3be4114a81436bd468f71b4a1ed31e16a18f6a81
AidanLam06 <aidanlam02@gmail.com>
2025-09-24 11:52:09 -0400

    Update radar.c

--- main/radar.c
+++ main/radar.c
@@ -9,43 +9,49 @@
 #include "driver/ledc.h"
 #include "freertos/queue.h"
 #include "driver/i2c.h"
+#include "esp_timer.h"
 
+//servo
 #define SERVO_CHANNEL LEDC_CHANNEL_0
 #define LEDC_MODE LEDC_LOW_SPEED_MODE
-#define SERVO_OUT (2)
+#define SERVO_OUT_PIN (2)
 #define LEDC_TIMER LEDC_TIMER_0
 #define LEDC_DUTY_RES LEDC_TIMER_14_BIT
 #define LEDC_FREQUENCY (50)
+#define MIN_DUTY (409) // floor(0.001/0.02 * 2^14)
+#define MAX_DUTY (1966) // floor(0.002/0.02 * 2^14)
+#define STEP (9)
+#define MAX_STEPS ((MAX_DUTY-MIN_DUTY)/STEP)
 
-#define PING_TIMEOUT 6000
-#define TRIG_HIGH_DELAY 10
-#define TRIG_LOW_DELAY 4
-#define TRIG_PIN 5
-#define ECHO_PIN 4
-#define MAX_SCAN_DISTANCE 200 // 200 cm, 2m
-#define CENTER_X 64
-#define CENTER_Y 64
+//ultrasonic
+#define PING_TIMEOUT_MS (6000)
+#define TRIG_HIGH_DELAY (10)
+#define TRIG_LOW_DELAY (4)
+#define TRIG_PIN (5)
+#define ECHO_PIN (4)
+#define MAX_SCAN_DISTANCE (200) // 200 cm, 2m
+#define CENTER_X (64)
+#define CENTER_Y (64)
 
-
+//I2C
 #define I2C_NUM I2C_NUM_0
-#define I2C_MASTER_FREQ_HZ 800000
+#define I2C_MASTER_FREQ_HZ (800000)
+#define SDA_PIN (21)
+#define SCL_PIN (22)
 
-#define PIXEL_LIFETIME_MS 750
+#define PIXEL_LIFETIME_MS (750)
 
-#define true 1
-#define false 0
+#define DESIRED_LOOP_DURATION_MS 8000 
 
+// stdbool.h refused to work after trying many different solutions
+#define true (1)
+#define false (0)
 
-static const char *tag_main = "MAIN";
-static const char *tag_ssd1306 = "SSD1306";
+static const char *tag = "MAIN";
 
-const int step = 9;
-const int min_duty = 409; // floor(0.001/0.02 * 2^14)
-const int max_duty = 1966; // floor(0.002/0.02 * 2^14)
-int duty = min_duty;
+int duty = MIN_DUTY;
 bool pos_direction = true;
 int step_count = 0;
-const int max_steps = (max_duty-min_duty)/step;
 const int iteration_time = 8;
 
 const uint8_t i2c_address = 0x3C; //address on the oled pcb is 0x78 which translates to 0x3C if you shift all the bits right by one bit
@@ -59,19 +65,21 @@ static SemaphoreHandle_t echo_semaphore;
 static int64_t echo_start = 0;
 static int64_t echo_end = 0;
 
-int64_t distance_history[max_steps];
+int64_t distance_history[MAX_STEPS];
 uint8_t frame_buffer[1024];
-uint32_t pixel_expiration[max_steps];
+uint32_t pixel_expiration[MAX_STEPS];
 
 typedef struct {
-    gpio_port_t echo_pin;
-    gpio_port_t trig_pin;
+    gpio_num_t echo_pin;
+    gpio_num_t trig_pin;
 } ultrasonic_sensor_t;
 
 typedef struct {
-    ultrasonic_sensor_t *sensor;
-    int64_t *time_us;
-} sensor_ping_task_args_t;
+    int64_t start;
+    int64_t end;
+} loop_timer_t;
+
+loop_timer_t loop_timer;
 
 typedef struct {
     int x;
@@ -107,9 +115,12 @@ static void IRAM_ATTR echo_isr_handler(void *args) {
     int level = gpio_get_level(ECHO_PIN);
     int64_t time_now = esp_timer_get_time();
 
-    if (level == 1) 
+    if (level == 1) {
+        ESP_LOGI(tag, "ISR: echo start");
         echo_start = time_now;
+    }
     else {
+        ESP_LOGI(tag, "ISR: echo stop");
         echo_end = time_now;
         BaseType_t xHigherPriorityTaskWoken = pdFALSE;
         xSemaphoreGiveFromISR(echo_semaphore, &xHigherPriorityTaskWoken);
@@ -121,19 +132,17 @@ static void IRAM_ATTR echo_isr_handler(void *args) {
 
 // ==================================================================================
 
+// =============================== FORWARD DECLARATIONS =============================
+
+void draw_bresenham_semicircle(uint8_t *buffer);
+void draw_bresenham_line(uint8_t *buffer, int x1, int y1, int x2, int y2);
+void draw_pixel(uint8_t *buffer, int distance, int x2, int y2);
+void ssd1306_write_page(i2c_port_t i2c_num, uint8_t addr, int page, int col, uint8_t *data_buffer, int len);
+
+// ==================================================================================
+
 // ====================================== INIT ======================================
 static void ledc_setup() {
-    ledc_channel_config_t ledc_channel = {
-        .speed_mode = LEDC_MODE,
-        .channel = SERVO_CHANNEL,
-        .timer_sel = LEDC_TIMER_0,
-        .intr_type = LEDC_INTR_DISABLE,
-        .gpio_num = SERVO_OUT,
-        .duty = 0,
-        .hpoint = 0
-    };
-    ledc_channel_config(&ledc_channel);
-
     ledc_timer_config_t ledc_timer = {
         .speed_mode = LEDC_MODE,
         .duty_resolution = LEDC_DUTY_RES,
@@ -142,6 +151,17 @@ static void ledc_setup() {
         .clk_cfg = LEDC_AUTO_CLK
     };
     ledc_timer_config(&ledc_timer);
+
+    ledc_channel_config_t ledc_channel = {
+        .speed_mode = LEDC_MODE,
+        .channel = SERVO_CHANNEL,
+        .timer_sel = LEDC_TIMER_0,
+        .intr_type = LEDC_INTR_DISABLE,
+        .gpio_num = SERVO_OUT_PIN,
+        .duty = 0,
+        .hpoint = 0
+    };
+    ledc_channel_config(&ledc_channel);
 }
 
 static void gpio_setup() {
@@ -166,7 +186,7 @@ static void gpio_setup() {
     gpio_isr_handler_add(ECHO_PIN, echo_isr_handler, NULL);
 }
 
-static void i2c_master_init(int16_t sda, int16_t scl, int16_t reset) {
+static void i2c_master_init(int16_t sda, int16_t scl) {
     i2c_config_t i2c_config = {
 		.mode = I2C_MODE_MASTER,
 		.sda_io_num = sda,
@@ -254,39 +274,41 @@ void servo_loop_task(void *args) {
 void servo_step_task(void *args) { // will trigger a single step in the servo every time it is called. I think iteration time should be applied in the main loop.
     for (;;) {
         if (xSemaphoreTake(oled_to_servo, portMAX_DELAY)) {
-            if (pos_direction) duty += step;
-            else duty -= step;
+            ESP_LOGI(tag, "servo_step");
+            if (pos_direction) duty += STEP;
+            else duty -= STEP;
 
             if (pos_direction) step_count++;
             else step_count--;
 
-            if (duty >= max_duty) pos_direction = false;
-            if (duty <= min_duty) pos_direction = true;
+            if (duty >= MAX_DUTY) pos_direction = false;
+            if (duty <= MIN_DUTY) pos_direction = true;
 
             ledc_set_duty(LEDC_MODE, SERVO_CHANNEL, duty);
             ledc_update_duty(LEDC_MODE, SERVO_CHANNEL);  
 
             xSemaphoreGive(servo_to_ultra);
 
-            vTaskDelay(iteration_time / portTICK_PERIOD_MS);
+            // vTaskDelay(iteration_time / portTICK_PERIOD_MS);
         }
     }
 }
 
-void sensor_ping_task(void *pvParameters) {
-    sensor_ping_task_args_t *args = (sensor_ping_task_args_t *)pvParameters;
-    ultrasonic_sensor_t *sensor = args->sensor;
-    int64_t *time_us = args->time_us;
+void sensor_ping_task(void *args) {
+    ultrasonic_sensor_t *sensor = (ultrasonic_sensor_t*)args;
 
     for (;;) {
         if (xSemaphoreTake(servo_to_ultra, portMAX_DELAY)) {
+            loop_timer.start = esp_timer_get_time(); // start the loop time
+            ESP_LOGI(tag, "sensor_ping");
             gpio_set_level(sensor->trig_pin, 0);
             esp_rom_delay_us(TRIG_LOW_DELAY);
             gpio_set_level(sensor->trig_pin,1);
             esp_rom_delay_us(TRIG_HIGH_DELAY);
             gpio_set_level(sensor->trig_pin, 0);
 
-            if (xSemaphoreTake(echo_semaphore, pdMS_TO_TICKS(PING_TIMEOUT))) {
+            if (xSemaphoreTake(echo_semaphore, pdMS_TO_TICKS(PING_TIMEOUT_MS))) {
+                ESP_LOGI(tag, "echo signal");
                 int64_t duration = echo_end - echo_start;
                 int distance_cm = (int)(duration/58); 
                 xQueueSend(ultra_to_oled, &distance_cm, portMAX_DELAY);
@@ -298,9 +320,12 @@ void sensor_ping_task(void *pvParameters) {
 void write_to_oled_task(void *args) {
     uint8_t *buffer = (uint8_t *)args;
     int distance;
+    int64_t cycle_time;
 
-    TickType_t prev_refresh = xTaskGetTickCount();
-    const TickType_t refresh = pdMS_TO_TICKS(33);
+    // TickType_t prev_refresh = xTaskGetTickCount();
+    // const TickType_t refresh = pdMS_TO_TICKS(33);
+
+    memset(frame_buffer, 0x00, sizeof(frame_buffer)); // reset frame_buffer to prepare for new data
 
     for (;;) {
         int x2 = coord_lookup_table[step_count].x;
@@ -310,34 +335,44 @@ void write_to_oled_task(void *args) {
         draw_bresenham_line(buffer, CENTER_X, CENTER_Y, x2, y2);
 
         if (xQueueReceive(ultra_to_oled, &distance, portMAX_DELAY)) {
-            if (abs(distance - distance_history[step_count]) > 4) {
+            ESP_LOGI(tag, "write_to_oled");
+            if (llabs(distance - distance_history[step_count]) > 4) {
                 distance_history[step_count] = distance;
                 pixel_expiration[step_count] = xTaskGetTickCount() + pdMS_TO_TICKS(750);
             }
-
         }
 
         uint32_t now = xTaskGetTickCount();
-        for (int step = 0; step < max_steps; step++) {
+        for (int step = 0; step < MAX_STEPS; step++) {
             if (pixel_expiration[step] > now) {
-                draw_pixel(buffer, distance, x2, y2);
+                draw_pixel(buffer, distance, coord_lookup_table[step].x, coord_lookup_table[step].y);
             }
         }
 
         for (int page = 0; page < 8; page++) {
             ssd1306_write_page(I2C_NUM_0, i2c_address, page, 0, &buffer[page*128], 128);
         }
-    vTaskDelayUntil(&prev_refresh, refresh);
+        loop_timer.end = esp_timer_get_time();
+        cycle_time = loop_timer.end - loop_timer.start;
+        vTaskDelay((DESIRED_LOOP_DURATION_MS - cycle_time)/ portTICK_PERIOD_MS); // fills remainder of desired loop duration using this calculation based on the cycle duration (sensor->oled). doesn't consider servo step because the lion doesn't concern himself with servo rotor stalling and assumes constant step time
+        xSemaphoreGive(oled_to_servo);
+        // vTaskDelayUntil(&prev_refresh, refresh);
     }
+}
+
+void startup_ritual_task(void *args) {
+    ESP_LOGI(tag, "startup_ritual");
+    xSemaphoreGive(servo_to_ultra);
+    vTaskDelete(NULL);
 }
 
 // ===================================================================================
 
 // =================================== FUNCTIONS =====================================
 
-void draw_bresenham_line(uint8_t *buffer, int x1, int y1, int *x2_ptr, int *y2_ptr) {
-    int y2 = *y2_ptr;
-    int x2 = *x2_ptr;
+void draw_bresenham_line(uint8_t *buffer, int x1, int y1, int x2, int y2) {
+    //int y2 = *y2_ptr;
+    //int x2 = *x2_ptr;
     int m_new = 2*(y2-y1);
     int slope_error_new = m_new - (x2-x1);
     int page, bit, index;
@@ -358,7 +393,6 @@ void draw_bresenham_line(uint8_t *buffer, int x1, int y1, int *x2_ptr, int *y2_p
 
 void draw_bresenham_semicircle(uint8_t *buffer) {
     // I only need (-y,-x), (-x,-y), (x,-y), (y, -x) since I 1) only want half the circle 2) the origin (0,0) is at the top left of the screen and the max is at the bottom right so any shape meant to be plotted on a typical graph would be inverted
-
     int r = 62;
     int x = 0, y = r;
     int d = 3 - 2*r;
@@ -393,7 +427,7 @@ void draw_bresenham_semicircle(uint8_t *buffer) {
 
 void draw_pixel(uint8_t *buffer, int distance, int x2, int y2) {
     // inside this function there needs to be the logic to find where the pixel should be placed and then also writing that pixel to the frame_buffer
-    float pix_radius_scale = distance/MAX_SCAN_DISTANCE; // this should find the position of the distance relative to the max scanning distance. Ex: distance is 50cm; 50/200 = 0.25
+    float pix_radius_scale = (float)distance/(float)MAX_SCAN_DISTANCE; // this should find the position of the distance relative to the max scanning distance. Ex: distance is 50cm; 50/200 = 0.25
     // now just find the spot that would be 25% along the line to the (x2,y2) from the centerpoint
     float x_f = CENTER_X + pix_radius_scale*(x2-CENTER_X);
     float y_f = CENTER_Y + pix_radius_scale*(y2-CENTER_Y);
@@ -434,8 +468,6 @@ void ssd1306_write_page(i2c_port_t i2c_num, uint8_t addr, int page, int col, uin
 // ==================================================================================
 
 void app_main(void) {
-    static int64_t time;
-
     echo_semaphore = xSemaphoreCreateBinary();
     oled_to_servo = xSemaphoreCreateBinary();
     ultra_to_oled = xQueueCreate(8, sizeof(int));
@@ -445,18 +477,16 @@ void app_main(void) {
         .trig_pin = TRIG_PIN,
         .echo_pin = ECHO_PIN
     };
-
-    static sensor_ping_task_args_t args = {
-        .sensor = &sensor,
-        .time_us = &time
-    };
     
     ledc_setup();
     gpio_setup();
 
     ultrasonic_init(&sensor);
+    i2c_master_init(SDA_PIN, SCL_PIN);
+    i2c_init(I2C_NUM, i2c_address);
 
     xTaskCreate(&servo_step_task, "servo_step_task", 2048, NULL, 5, NULL);
-    xTaskCreate(&sensor_ping_task, "sensor_ping_task", 2048, &args, 5, NULL);
+    xTaskCreate(&sensor_ping_task, "sensor_ping_task", 2048, &sensor, 5, NULL);
     xTaskCreate(&write_to_oled_task, "write_to_oled_task", 2048, &frame_buffer, 5, NULL);
+    xTaskCreate(&startup_ritual_task, "startup_ritual_task", 2048, NULL, 5, NULL);
 }
